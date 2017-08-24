f66eac40805a6e1a9899026e27e99507ad03dc17
Lucas Jensen <lukejensen@sanbrunocable.com>
2017-08-24 09:32:30 -0700

    Forgot my other program, decided to push this one too

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,10 @@
+/*
+ * Prints "Hello World!"
+ * @Lucas Jensen
+ * @8/21/2017
+ */
+public class Hello_World {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
