9be4714889f141eecc92da408a4f5305a98d419e
Lucas Jensen <lukejensen@sanbrunocable.com>
2017-08-24 09:30:42 -0700

    Just throwing things at a cloud and hoping this works.

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,14 @@
+//Lucas Jensen
+//8-23-2017
+//Project 2 -- use Print and println
+public class Project_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Line 1");
+		System.out.println("Line Beta");
+		System.out.println("Line C ");
+		System.out.print("Consistency is important.");
+		
+	}
+
+}
