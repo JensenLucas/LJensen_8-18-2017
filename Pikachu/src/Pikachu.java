f77ad1b96e16f7b753b807a1a6782bc79ebec4bd
Lucas Jensen <lukejensen@sanbrunocable.com>
2017-08-30 10:31:59 -0700

    Pikachu

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,11 @@
+//Lucas Jensen
+//8-28-2017
+//Pikachu says hi
+public class Pikachu {
+	public static void main(String[] args) {
+		System.out.println("		Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("				   (\\__/)");
+		System.out.println("  				   (o^.^)");
+		System.out.println("				z(_(\")(\")");		
+	}
+}
