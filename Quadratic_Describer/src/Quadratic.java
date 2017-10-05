55ff7499a6964d1fd5f2269abf4cfcad5d13568c
Lucas Jensen <lukejensen@sanbrunocable.com>
2017-10-05 09:03:06 -0700

    Quadratic Describer first push; may have screwed up this push

---
+++ Quadratic_Describer/src/Quadratic.java
@@ -0,0 +1,9 @@
+/* Lucas Jensen
+ * 9/27/2017
+ * library for QuadraticClient; contains quadrDescriber and other methods
+ */
+public class Quadratic {
+	public static String quadrDescriber(double opperandA, double opperandB, double opperandC) {
+		return(null);
+	}
+}
---
+++ Quadratic_Describer/src/QuadraticClient.java
@@ -0,0 +1,8 @@
+/* Lucas Jensen
+ * 9/27/2017
+ * accesses Quadratic.java
+ * handles user input and interface
+ */
+public class QuadraticClient {
+
+}
