c8cb03727b5510d43a7077820755dd5d20a0737c
Lucas Jensen <lukejensen@sanbrunocable.com>
2017-10-27 09:51:26 -0700

    SPlit or somethigs

--- Split/src/Split.java
+++ Split/src/Split.java
@@ -1,11 +1,35 @@
+//Lucas Jensen
+//10-27-2017
+//Split stuff
+
+import java.util.*;
 
 public class Split {
 	public static void main (String[] args) {
-		System.out.println("I like apples!".split(" ")); 
+		//Part 1
+		/*System.out.println(Arrays.toString("I like apples!".split(" ")));
 //			it will split at spaces and return an array of ["I","like","apples!"]
-		// Example 2: "I really like really red apples".split("really")
+		System.out.println(Arrays.toString("I really like really red apples".split("really")));
 //			it will split at the word "really" and return an array of ["I "," like "," red apples!"]
 		//play around with String.split! 
-		//What happens if you "I reallyreally likeapples".split("really") ?
+		System.out.println(Arrays.toString("I reallyreally likeapples".split("really"))); //Testing 2 "really"s
+		*/
+		
+		//Part 2
+		System.out.println(filling("breadcheesepotatobreadcakebread"));
+		System.out.println(filling("applespineapplesbreadlettustomatobaconmayohambreadcheese"));
+		System.out.println(filling("checkbreadtestbreadpiebreadthirdbreadcheck"));
+		
+	}
+	public static String filling(String sandwhich) {
+		String[] operand = (sandwhich).split("bread");
+		if(operand.length < 2) {
+			return("Invalid sandwhich");
+		}
+		String fillings = ("");
+		for(int i=1; i<operand.length-1; i++) {
+			fillings += operand[i];
+		}
+		return(fillings);
 	}
 }
